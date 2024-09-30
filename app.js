236f0e216f3772d93cbeaa9bb95178c3dd019b1a
Gabo.Dev <55933625+GaboDev-1024@users.noreply.github.com>
2024-09-29 21:58:32 -0300

    Update morgan app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ express = require('express');
 app = express();
 indexRoutes = require('./Router/index');
 usersRoutes = require('./Router/users');
+const morgan = require('morgan');
 
 //CONFIG'S
 app.use('/', indexRoutes);
 app.use('/users', usersRoutes);
 app.use('/assets', express.static(__dirname + '/Public'));
 app.use(express.static('Public'));
+app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
